8089dec5e8be00930841e155c3728c06ff38823f
Martin Herold <martin3421@gmail.com>
2021-05-02 22:45:20 +0200

    new service, component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PhotoPageComponent } from './photo-page/photo-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoPageComponent
   ],
   imports: [
     BrowserModule
